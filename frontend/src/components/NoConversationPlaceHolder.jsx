69da47e3a87952b39c9b51425f368817085cb908
SakshiBorse0712 <sakshiborse.0712@gmail.com>
2026-03-14 18:56:51 +0530

    error in file name

--- frontend/src/components/NoConversationPlaceHolder.jsx
+++ frontend/src/components/NoConversationPlaceHolder.jsx
@@ -1,6 +1,6 @@
 import { MessageCircleIcon } from "lucide-react";
 
-const NoConversationPlaceholder = () => {
+const NoConversationPlaceHolder = () => {
   return (
     <div className="flex flex-col items-center justify-center h-full text-center p-6">
       <div className="size-20 bg-cyan-500/20 rounded-full flex items-center justify-center mb-6">
@@ -14,4 +14,4 @@ const NoConversationPlaceholder = () => {
   );
 };
 
-export default NoConversationPlaceholder;
+export default NoConversationPlaceHolder;
